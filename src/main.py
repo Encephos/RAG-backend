f35f3a092fd9f116a62185b03277379ba2b37cb3
sornyu <oskar.sattler@icloud.com>
2025-12-12 17:31:05 +0100

    .

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=["*"], # Allow ALL origins for now to fix CORS issues
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
